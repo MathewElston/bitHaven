a38b3e6dba417a526968a33e3fe3e1a7a3f652a4
Mathew Elston <elstonmathew@yahoo.com>
2023-03-11 10:59:06 -0800

    added cookie page will falling cookies, that save as a cookie when clicked

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.body.style.marginLeft = "0";
 }
+
+function getCookieValue(name) {
+  let cookies = document.cookie.split("; ");
+  for (let i=0; i< cookies.length; i++) {
+      let cookie = cookies[i].split("=");
+      if (cookie[0]=== name) {
+          return cookie[1];
+      }
+  }
+  return 0;
+}
+let cookieCount = document.getElementById("cookieCount");
+
+window.addEventListener("load", function() {
+  cookieCount.textContent = getCookieValue("cookieCount");
+});
